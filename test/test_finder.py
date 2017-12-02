0a8cb34f73410e4050daa004ab9397244c9f6be5
Jason Simeone <jay@classless.net>
2017-12-02 15:34:47 -0500

    Test fixes

--- test/test_finder.py
+++ test/test_finder.py
@@ -15,7 +15,7 @@ def fix_paths(paths):
     return paths
 
 def test_exclude():
-    if os.mkfifo and not os.path.exists('test/project1/testfifo'):
+    if hasattr(os, 'mkfifo') and not os.path.exists('test/project1/testfifo'):
         os.mkfifo('test/project1/testfifo')
 
     cfg = get_default_config()
--- test/test_setuptools.py
+++ test/test_setuptools.py
@@ -1,13 +1,18 @@
 
+import sys
 import subprocess
 
+import pytest
 
+
+@pytest.mark.skipif(sys.platform == 'win32', reason='windows hates setuptools')
 def test_default():
     proc = subprocess.Popen(
         ['python', 'setup.py', 'tidypy'],
         cwd='test/project1',
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
+        universal_newlines=True,
     )
 
     out, err = proc.communicate()
@@ -15,11 +20,13 @@ def test_default():
     assert proc.returncode == 0
 
 
+@pytest.mark.skipif(sys.platform == 'win32', reason='windows hates setuptools')
 def test_options():
     proc = subprocess.Popen(
         ['python', 'setup.py', 'tidypy', '--fail-on-issue', '--project-path=test/project1'],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
+        universal_newlines=True,
     )
 
     out, err = proc.communicate()
